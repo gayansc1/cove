7bb4a7642535f560e623f4fa830dfc5faa8279c8
gayansc1 <wgschamarasinghe@gmail.com>
2023-07-30 09:39:37 +0530

    Initial commit with improvements

--- bad.js
+++ bad.js
@@ -1,66 +1,65 @@
-/**
- * Please, improve this component and fix all problems.
- *
- * What is important:
- * - design (extensibility, testability)
- * - code cleanliness, following best practices
- * - bugs
- * - consistency
- * - naming
- * - formatting
- *
- * Write your perfect code!
- */
-
 import React, { useEffect, useState } from 'react';
 
-function Card({title,text,target,linkTitle,href,rel,onClick,linkClassName})
-{
+function Card({ title, text, target, linkTitle, href, rel, onClick, linkClassName }) {
   return (
     <div className="card">
-
-        <div className="card__title">{title}</div>
-
-        <div className="card__text">{text}</div>
-        <a className={`default-link card__link ${linkClassName}`} target={target} rel={rel} href={href} onClick={onClick}>
-          {linkTitle}
-        </a>
+      <div className="card__title">{title}</div>
+      <div className="card__text">{text}</div>
+      <a
+        className={`default-link card__link ${linkClassName}`}
+        target={target}
+        rel={rel}
+        href={href}
+        onClick={() => onClick(href)}
+      >
+        {linkTitle}
+      </a>
     </div>
   );
 }
 
-export default function Page () {
-  const [cards, setCards] = useState();
+export default function Page() {
+  const [cards, setCards] = useState([]);
 
-  useEffect(async () => {
-    var data = await fetch('https://my-json-server.typicode.com/savayer/demo/posts');
-    let json = data.json();
-
-    let newData;
-    json.forEach((item) => {
-      newData.id = item.id;
-      newData.title = item.title;
-      newData.link_title = item.link_title;
-      newData.link = item.link;
-      newData.text = item.body.en.substr(0, 50) + '...';
-    });
-
-    setCards(newData);
-  });
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch('https://my-json-server.typicode.com/savayer/demo/posts');
+        const data = await response.json();
+        const transformedData = data.map((item) => ({
+          id: item.id,
+          title: item.title.en,
+          link_title: item.link_title,
+          link: item.link,
+          text: item.body.en.substr(0, 50) + '...',
+        }));
+        setCards(transformedData);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    }
+    fetchData();
+  }, []); // <-- Added an empty dependency array
 
   function analyticsTrackClick(url) {
-    // sending clicked link url to analytics
+    // Sending clicked link URL to analytics
     console.log(url);
   }
 
   return (
     <div>
-    {cards.map(function (item) {
-      return (
-        <Card title={item.title.en} linkTitle={item.link_title} href={item.link} text={item.text} linkClassName={item.id == 1 ? 'card__link--red' : ''} target={item.id == 1 ? '_blank' : ''}
-          onClick={analyticsTrackClick(item.link)} />
-      );
-    })}
+      {cards.map((item) => (
+        <Card
+          key={item.id}
+          title={item.title}
+          linkTitle={item.link_title}
+          href={item.link}
+          text={item.text}
+          linkClassName={item.id === 1 ? 'card__link--red' : ''}
+          target={item.id === 1 ? '_blank' : ''}
+          onClick={analyticsTrackClick}
+        />
+      ))}
     </div>
   );
 }
